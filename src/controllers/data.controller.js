e7561d64f7b84a5069b1916233a014efc99fc241
Nazmul Huda <nznazmulhuda04@gmail.com>
2024-08-15 17:32:49 +0600

    get pagination data by send page number and limit number in query

--- src/controllers/data.controller.js
+++ src/controllers/data.controller.js
@@ -13,3 +13,19 @@ export const singleProduct = async (req, res) => {
   });
   res.send(product);
 };
+
+export const paginations = async (req, res) => {
+  let { page = 1, limit = 10 } = req.query; // get page number and limit from query
+
+  page = Number(page); // convert page number to number
+  limit = Number(limit); // convert limit number to number
+
+  const startIndex = (page - 1) * limit; // get start index
+  const products = await dataCollection
+    .find()
+    .skip(startIndex)
+    .limit(limit)
+    .toArray();
+
+  res.send(products);
+};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { getAllData, singleProduct } from "../controllers/data.controller.js";
+import {
+  getAllData,
+  paginations,
+  singleProduct,
+} from "../controllers/data.controller.js";
 
 const router = Router();
 
 // Define the routes
-router.route("/get-all-data").get(getAllData);
-router.route("/product").get(singleProduct);
+router.route("/get_all_products").get(getAllData); // get all products
+router.route("/product").get(singleProduct); // get single product
+router.route("/paginations").get(paginations); // get paginations data
 
 export default router;
