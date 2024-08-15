b060ca19d562c2c9686391aac548d87df1e24cea
Nazmul Huda <nznazmulhuda04@gmail.com>
2024-08-15 23:36:07 +0600

    get minimum and maximum price

--- src/routes/data.routes.js
+++ src/routes/data.routes.js
@@ -12,6 +12,6 @@ const router = Router();
 router.route("/get_all_products").get(getAllData); // get all products
 router.route("/product").get(singleProduct); // get single product
 router.route("/paginations").get(paginations); // get paginations data
-router.route("/price_range").get(priceRange);
+router.route("/price_range").get(priceRange); // get maximum and minimum price
 
 export default router;
